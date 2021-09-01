218b99b2a54567aa696a2b8e765a03e92ebd8185
Nick Pieros <nick.pieros@netfoundry.io>
2021-09-01 20:04:04 +0000

    added main.go
    removed flags
    
    updated cmd to allow for passing in a predefined set of ConfigFlags for use when creating the command

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 // This is a generated file. Do not edit directly.
 
-module k8s.io/kubectl
+module github.com/openziti-incubator/kubectl
 
 go 1.16
 
@@ -15,7 +15,7 @@ require (
 	github.com/fatih/camelcase v1.0.0
 	github.com/fvbommel/sortorder v1.0.1
 	github.com/golangplus/testing v0.0.0-20180327235837-af21d9c3145e // indirect
-	github.com/google/go-cmp v0.5.5
+	github.com/google/go-cmp v0.5.6
 	github.com/googleapis/gnostic v0.5.5
 	github.com/jonboulle/clockwork v0.2.2
 	github.com/liggitt/tabwriter v0.0.0-20181228230101-89fcab3d43de
@@ -25,22 +25,24 @@ require (
 	github.com/onsi/ginkgo v1.14.0
 	github.com/onsi/gomega v1.10.1
 	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/openziti/sdk-golang v0.15.86
 	github.com/russross/blackfriday v1.5.2
-	github.com/sirupsen/logrus v1.6.0
+	github.com/sirupsen/logrus v1.8.1
 	github.com/spf13/cobra v1.2.1
 	github.com/spf13/pflag v1.0.5
 	github.com/stretchr/testify v1.7.0
 	golang.org/x/sys v0.0.0-20210820121016-41cdb8703e55
 	gopkg.in/yaml.v2 v2.4.0
-	k8s.io/api v0.0.0-20210825040442-f20796d02069
-	k8s.io/apimachinery v0.0.0-20210825040238-74be3b88bedb
-	k8s.io/cli-runtime v0.0.0-20210825042947-c992623183f8
-	k8s.io/client-go v0.0.0-20210825040738-3dc80a3333cd
-	k8s.io/component-base v0.0.0-20210825041339-a33683002a90
-	k8s.io/component-helpers v0.0.0-20210825041451-6721137b4907
+	k8s.io/api v0.22.1
+	k8s.io/apimachinery v0.22.1
+	k8s.io/cli-runtime v0.22.1
+	k8s.io/client-go v0.22.1
+	k8s.io/component-base v0.22.1
+	k8s.io/component-helpers v0.22.1
 	k8s.io/klog/v2 v2.9.0
 	k8s.io/kube-openapi v0.0.0-20210817084001-7fbd8d59e5b8
-	k8s.io/metrics v0.0.0-20210825042829-1468ab25f472
+	k8s.io/kubectl v0.22.1
+	k8s.io/metrics v0.22.1
 	k8s.io/utils v0.0.0-20210819203725-bdf08cb9a70a
 	sigs.k8s.io/kustomize/kustomize/v4 v4.2.0
 	sigs.k8s.io/kustomize/kyaml v0.11.0
---
+++ main.go
@@ -0,0 +1,101 @@
+/*
+Copyright 2014 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"context"
+	"math/rand"
+	"net"
+	"os"
+	"time"
+
+	"github.com/openziti-incubator/kubectl/pkg/cmd"
+	"github.com/openziti-incubator/kubectl/pkg/cmd/plugin"
+	"github.com/spf13/cobra"
+
+	"github.com/openziti/sdk-golang/ziti"
+	"github.com/openziti/sdk-golang/ziti/config"
+
+	"github.com/openziti-incubator/kubectl/pkg/util/logs"
+	"github.com/sirupsen/logrus"
+
+	// Import to initialize client auth plugins.
+	"k8s.io/cli-runtime/pkg/genericclioptions"
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
+	"k8s.io/client-go/rest"
+)
+
+var configFilePath string
+var serviceName string
+
+type ZitiFlags struct {
+	ZConfig string
+	Service string
+}
+
+var zFlags = ZitiFlags{}
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	kubeConfigFlags := genericclioptions.NewConfigFlags(true).WithDeprecatedPasswordFlag()
+	kubeConfigFlags.WrapConfigFn = wrapConfigFn
+
+	command := cmd.NewDefaultKubectlCommandWithArgs(cmd.NewDefaultPluginHandler(plugin.ValidPluginFilenamePrefixes), os.Args, os.Stdin, os.Stdout, os.Stderr, kubeConfigFlags)
+	command = setZitiFlags(command)
+	command.PersistentFlags().Parse(os.Args)
+
+	configFilePath = command.Flag("ZConfig").Value.String()
+	serviceName = command.Flag("Service").Value.String()
+
+	// TODO: once we switch everything over to Cobra commands, we can go back to calling
+	// cliflag.InitFlags() (by removing its pflag.Parse() call). For now, we have to set the
+	// normalize func and add the go flag set by hand.
+
+	logs.InitLogs()
+	defer logs.FlushLogs()
+
+	if err := command.Execute(); err != nil {
+		os.Exit(1)
+	}
+}
+
+func dialFunc(ctx context.Context, network, address string) (net.Conn, error) {
+	service := serviceName
+	configFile, err := config.NewFromFile(configFilePath)
+
+	if err != nil {
+		logrus.WithError(err).Error("Error loading config file")
+		os.Exit(1)
+	}
+
+	context := ziti.NewContextWithConfig(configFile)
+	return context.Dial(service)
+}
+
+func wrapConfigFn(restConfig *rest.Config) *rest.Config {
+
+	restConfig.Dial = dialFunc
+	return restConfig
+}
+
+func setZitiFlags(command *cobra.Command) *cobra.Command {
+
+	command.PersistentFlags().StringVarP(&zFlags.ZConfig, "ZConfig", "c", "", "Path to ziti config file")
+	command.PersistentFlags().StringVarP(&zFlags.Service, "Service", "S", "", "Service name")
+
+	return command
+}
